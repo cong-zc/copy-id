fcfddeda5b2666b8214b61d5d9fc98bbb8082a91
cong-zc <zc961123@outlook.com>
2024-10-22 21:52:50 +0800

    实现基础功能
    点击按钮复制edge或chrome扩展到剪切板

--- popup.tsx
+++ popup.tsx
@@ -1,24 +1,55 @@
-import { useState } from "react"
+import 'index.css'
+// import { sendToBackground } from '@plasmohq/messaging';
+const 点击获取url = async () => {
 
-function IndexPopup() {
-  const [data, setData] = useState("")
+  await chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    if (tabs.length > 0) {
+      const 地址 = tabs[0].url
+      const id = 地址.split('?')[0].split('/').pop()
+      if (id !== null) {
+        复制文本内容(id)
+      } else {
+        return
+      }
+    }
+  });
+
+}
+
+const 复制文本内容 = async (text: string) => {
+  try {
+    await navigator.clipboard.writeText(text);
+    console.log('文本已复制到剪贴板');
+    // 向后台传递消息
+    // const resp = await sendToBackground({
+    //   name: "ping",
+    //   body: {
+    //     id: 123
+    //   }
+    // })
+    // console.log(resp)
+
+    // 创建通知
+    // chrome.notifications.create('0000', {
+    //   type: 'basic',
+    //   iconUrl: 'icon.png',
+    //   title: '复制成功',
+    //   message: '扩展id已复制到剪贴板'
+    // }, 通知id => {
+    //   console.log(通知id)
+    // })
+  } catch (err) {
+   
+    console.error('复制文本到剪贴板时出错:', err);
+  }
+}
+
+
+const IndexPopup = () => {
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="popup">
+      <span onClick={点击获取url}>复制扩展id</span>
     </div>
   )
 }
